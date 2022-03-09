6155b8e258c9df41e909d457751613b45f98b528
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-03-08 23:23:12 -0300

    adicionado arquivos de configuração.

---
+++ app/src/main/java/br/com/java/app_firebase_loja_online_master/ui/RegistrarActivity.java
@@ -0,0 +1,19 @@
+package br.com.java.app_firebase_loja_online_master.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import br.com.java.app_firebase_loja_online_master.R;
+
+public class RegistrarActivity extends AppCompatActivity {
+
+    private FrameLayout frameLayout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar);
+    }
+}
